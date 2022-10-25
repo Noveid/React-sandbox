50c9a30ab8f480b6c247057e96feb49ce9638695
Oleksandr L <noveidofficial@gmail.com>
2022-10-25 14:08:20 +0300

    add prettier, add theme for chakra-ui, add _document, add test page

---
+++ pages/test/chakra-ui.tsx
@@ -0,0 +1,8 @@
+import { Flex } from '@chakra-ui/react';
+
+export const ChakraUiPage = () => {
+  const testString = 'dasd';
+  return <Flex>Testing Chakra Flex component</Flex>;
+};
+
+export default ChakraUiPage;
